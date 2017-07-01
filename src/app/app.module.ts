09055dbf0900cc748fa0d7e576824b6f2865d251
Tom Bartolucci <tbartolucci@billtrust.com>
2017-07-01 17:49:01 -0400

    Hero Tour Angular tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule  // <-- Import to allow ngModel
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
